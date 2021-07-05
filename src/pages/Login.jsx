01a218f48b81e64e2fe1d46b5ad9fc38eaf8921f
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-07-05 02:15:09 -0300

    login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import api from "../services/api";
+import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -10,18 +11,18 @@ function Login() {
   const [disableButton, setDisableButton] = useState(true);
 
   const history = useHistory();
-  
-  function redirect (pathname, state) {
+
+  function redirect(pathname, state) {
     history.push({ pathname, state });
   }
 
-  function validateForm () {
+  function validateForm() {
     // eslint-disable-next-line no-useless-escape
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return !(regex.test(String(email).toLowerCase()) && password.length >= 6);
   }
 
-  async function handleSubmit (event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const response = await api.login(email, password);
 
@@ -45,44 +46,57 @@ function Login() {
 
   useEffect(() => {
     if (userInfo.authenticated) {
-      localStorage.setItem("user",  JSON.stringify({ ...userInfo.user }));
+      localStorage.setItem("user", JSON.stringify({ ...userInfo.user }));
       redirect("/wall", { ...userInfo });
     }
   }, [userInfo]);
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      {error}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <br />
-        <input
-          id="email"
-          onChange={(event) => setEmail(event.target.value)}
-          type="email"
-          placeholder="Email"
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <br />
-        <input
-          id="password"
-          onChange={(event) => setPassword(event.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <button disabled={disableButton} type="submit">
-          Login
-        </button>
-      </form>
-      <button type="button" onClick={() => redirect("/register")}>
-        Register
-      </button>
-      <button type="button" onClick={() => redirect("/wall", { ...userInfo })}>
-        Enter as a guest
-      </button>
+    <div className="my-container">
+      <div className="container">
+        <div className="row">
+        <div className="col"></div>
+          <form className="col-sm-4 align-self-center shadow p-3 mb-5 bg-body rounded" onSubmit={handleSubmit}>
+            {error}
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                id="email"
+                className="form-control"
+                onChange={(event) => setEmail(event.target.value)}
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                id="password"
+                className="form-control"
+                onChange={(event) => setPassword(event.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="d-flex justify-content-between ">
+              <button className="btn btn-primary" disabled={disableButton} type="submit">
+                Login
+              </button>
+              <button className="btn btn-primary" type="button" onClick={() => redirect("/register")}>
+                Register
+              </button>
+              <button className="btn btn-primary"  type="button" onClick={() => redirect("/wall", { ...userInfo })}>
+                Enter as a guest
+              </button>
+            </div>
+          </form>
+          <div className="col"></div>
+        </div>
+      </div>
     </div>
   );
 }
